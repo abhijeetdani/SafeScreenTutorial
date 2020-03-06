999d27d54f51065953f12b6b822f4ea60d28dea8
Abhijeet Dani <abhijeetdani@gmail.com>
2020-03-06 16:05:35 +0530

    adding the safe screen tutorial unity project

---
+++ Assets/Scenes/SafeAreaDetection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafeAreaDetection : MonoBehaviour {
+
+	public delegate void SafeAreaChanged(Rect safeArea);
+	public static event SafeAreaChanged OnSafeAreaChanged;
+
+	private Rect _safeArea;
+
+	private void Awake() {
+		_safeArea = Screen.safeArea;
+	}
+
+	private void Update() {
+		
+		if (_safeArea != Screen.safeArea) {
+			_safeArea = Screen.safeArea;
+			OnSafeAreaChanged?.Invoke(_safeArea);
+		}
+	}
+}
---
+++ Assets/Scenes/SafeAreaPanel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafeAreaPanel : MonoBehaviour {
+
+	private RectTransform _rectTransform;
+
+	private void Awake() {
+		_rectTransform = GetComponent<RectTransform>();
+		RefreshPanel(Screen.safeArea);
+	}
+
+	private void OnEnable() {
+		SafeAreaDetection.OnSafeAreaChanged += RefreshPanel;
+	}
+
+	private void OnDisable() {
+		SafeAreaDetection.OnSafeAreaChanged -= RefreshPanel;
+	}
+
+	private void RefreshPanel(Rect safeArea) {
+
+		Vector2 anchorMin = safeArea.position;
+		Vector2 anchorMax = safeArea.position + safeArea.size;
+
+		anchorMin.x /= Screen.width;
+		anchorMin.y /= Screen.height;
+		anchorMax.x /= Screen.width;
+		anchorMax.y /= Screen.height;
+
+		_rectTransform.anchorMin = anchorMin;
+		_rectTransform.anchorMax = anchorMax;
+	}
+}
